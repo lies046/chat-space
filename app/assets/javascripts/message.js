051034f62b2fdccc343aed94ee9c00e6866a190b
lies046 <skytree.skysky@gmail.com>
2019-12-01 16:36:13 +0900

    update

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,5 @@
-$(function(){
+$(function(){  
+
   function buildHTML(message){
     img = ( message.image ) ? `<img src=${message.image} >` : "";
       var html =
@@ -43,4 +44,44 @@ $(function(){
   });
   return false;
 });
-})
+
+var reloadMessages = function() {
+  last_message_id = $('.message:last').data("message-id");
+  $.ajax({
+    url: /\/groups\/\d+\/messages/,
+    type: 'get',
+    dataType: 'json',
+    data: {id: last_message_id}
+  })
+  .done(function(messages) {
+    console.log('success');
+  })
+  .fail(function() {
+    console.log('error');
+  });
+};
+
+var reloadMessages = function() {
+  if (window.location.href.match(/\/groups\/\d+\/messages/)){
+  last_message_id = $('.message:last').data("message-id");
+  $.ajax({
+    url: "api/messages",
+    type: 'get',
+    dataType: 'json',
+    data: {id: last_message_id}
+  })
+  .done(function(messages) {
+    var insertHTML = '';
+    $.each(messages, function(i, message) {
+      insertHTML += buildHTML(message)
+    });
+    $('.messages').append(insertHTML);
+    $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
+  })
+  .fail(function() {
+    console.log('error');
+  });
+  }
+};
+  setInterval(reloadMessages, 5000);
+});
